3aed4afa453e788f161650b1dac7806503df826a
trademm <123472330+trademm@users.noreply.github.com>
2023-02-09 18:35:02 +0100

    Add files via upload

---
+++ 45_nested list.py
@@ -0,0 +1,12 @@
+# spojovani listu
+
+list_1 = ["David", "Lukki", "Tom"]
+list_2 = ["Novy", "Jurko", "ukil"]
+
+number = len(list_1)
+print(number)
+
+# nested list 
+studenti = [list_1, list_2]
+print(studenti)
+print(studenti[0][1]) # vypise z listu 0 druhe jmeno
